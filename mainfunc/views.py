4a1f7375d544511e2ec4a7e4ef2ef4543651c715
DeveloperYun <yhb7588@naver.com>
2022-11-08 18:10:13 +0900

    Update views.py

--- mainfunc/views.py
+++ mainfunc/views.py
@@ -146,7 +146,9 @@ def main(request):
         cv2.imwrite("static/img/result.png",result)
         break
 
-    # 배경제거 알고리즘 적용.
+    ####################################
+    #FIXME: GRABCUT 알고리즘으로 배경 자체를 지워버리는 방법
+   
     # imageUrl = 'test.png'
     # imgres = cv2.imread(imageUrl)
 
@@ -166,9 +168,57 @@ def main(request):
     # b, g, r = cv2.split(imgres)
     # rgba = [b,g,r, alpha]
     # dst = cv2.merge(rgba,4)
-
+    
+    
     # cv2.imwrite("static/img/result.png", dst)
-
+    # #########################################
+    '''
+    # canny edge를 통해 경계선 찾고 색 채우기 방식을 적용한 것
+    BLUR = 21
+    CANNY_THRESH_1 = 18
+    CANNY_THRESH_2 = 28
+    MASK_DILATE_ITER = 2
+    MASK_ERODE_ITER = 2
+    MASK_COLOR = (0,0,0) # In BGR format
+    
+    
+    img = cv2.imread('test.png')
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    
+    edges = cv2.Canny(gray, CANNY_THRESH_1, CANNY_THRESH_2)
+    edges = cv2.dilate(edges, None)
+    edges = cv2.erode(edges, None)
+    
+    contour_info = []
+    contours, _ = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+    
+    for c in contours:
+        contour_info.append((
+            c,
+            cv2.isContourConvex(c),
+            cv2.contourArea(c),
+        ))
+    
+    contour_info = sorted(contour_info, key=lambda c: c[2], reverse=True)
+    max_contour = contour_info[0]
+    
+    mask = np.zeros(edges.shape)
+    cv2.fillConvexPoly(mask, max_contour[0], (255))
+    
+    mask = cv2.dilate(mask, None, iterations=MASK_DILATE_ITER)
+    mask = cv2.erode(mask, None, iterations=MASK_ERODE_ITER)
+    mask = cv2.GaussianBlur(mask, (BLUR, BLUR), 0)
+    mask_stack = np.dstack([mask]*3)
+    
+    mask_stack  = mask_stack.astype('float32') / 255.0
+    img         = img.astype('float32') / 255.0
+    
+    masked = (mask_stack * img) + ((1-mask_stack) * MASK_COLOR)
+    masked = (masked * 255).astype('uint8')
+    
+    dst = cv2.resize(masked, dsize=(640, 480), interpolation=cv2.INTER_AREA)
+    cv2.imwrite("static/img/result.png", dst)
+    '''
     return render(request, 'mainfunc/main.html')
 
 def main2(request):
